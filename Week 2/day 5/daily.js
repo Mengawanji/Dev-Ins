a3e52f7b10c7a3ec7adbf5d99533b98b30c5cb43
Mengawanji <111868855+Mengawanji@users.noreply.github.com>
2022-09-05 10:48:59 +0100

    Add files via upload

---
+++ Week 2/day 5/daily.js
@@ -0,0 +1,41 @@
+function playTheGame(){
+      let  user= confirm("Would you like to play?");
+         if user=false {
+            alrert("No problem, goodbye");
+        }else if{
+            user.isNaN("user")
+            alert("sorry Not a number, Goodbye");        
+        }else if{
+            if user>=11
+            alrert("Sorry it’s not a good number, Goodbye");
+        }else{
+           var computerNumber=(Math.floor(Math.random()*11) 
+            if (user>=0 || user <=10)
+            console.log(computerNumber) ;
+        }
+}
+playTheGame();
+//.....................................................
+
+   function compareNumbers(userNumber,computerNumber){
+            if userNumber=computerNumber{
+               alrert("WINNER")
+            }else if{
+               userNumber>computerNumber
+               prompt("Your number is bigger then the computer’s, guess again");
+            }else if{
+               userNumber<computerNumber
+               prompt("Your number is smaller then the computer’s, guess again");
+            }else{
+                 alert("out of chances")  
+         }
+      }
+      compareNumbers();
+
+
+
+
+
+
+
+
